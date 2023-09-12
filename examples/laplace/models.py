1f54d545b59445056c7c78f5fa5657091f00d0c1
felixagren97 <felixagren@outlook.com>
2023-09-12 13:03:24 +0200

    fixed gradients

--- examples/laplace/models.py
+++ examples/laplace/models.py
@@ -36,8 +36,8 @@ class Laplace(ForwardIVP):
         return u[0]
 
     def r_net(self, params, r):
-        du_r = grad(self.u_net)(params, r, argnums=1)
-        du_rr = grad(grad(self.u_net, argnums=1), argnums=1)(params, r) # Don't need to use hessian b/c scalar f and r        
+        du_r = grad(self.u_net)(params, r)
+        du_rr = grad(grad(self.u_net))(params, r) # Don't need to use hessian b/c scalar f and r        
         return r * du_rr + du_r  # Scaled by r, try w/o? 
 
     @partial(jit, static_argnums=(0,))
