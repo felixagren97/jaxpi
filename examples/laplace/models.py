bug investigation gradients

examples/laplace/models.py
@@ -36,8 +36,12 @@ class Laplace(ForwardIVP):
         return u[0]
 
     def r_net(self, params, r):
+        print('###########')
+        print('r', r)
         du_r = grad(self.u_net)(params, r)
         du_rr = grad(grad(self.u_net))(params, r) # Don't need to use hessian b/c scalar f and r        
+        print('du_r', du_r, 'du_rr', du_rr)
+        print('r * du_rr + du_r ', r * du_rr + du_r)
         return r * du_rr + du_r  # Scaled by r, try w/o? 
 
     @partial(jit, static_argnums=(0,))
@@ -65,7 +69,7 @@ class Laplace(ForwardIVP):
             res_loss = jnp.mean(l * w)
         else:
             #r_pred = vmap(self.r_net, (None, 0, 0))(params, batch[:, 0], batch[:, 1])
-            r_pred = vmap(self.r_net, (None, 0))(params, batch) #tried shifting to just batch
+            r_pred = vmap(self.r_net, (None, 0))(params, batch[:,0]) #tried shifting to just batch
             res_loss = jnp.mean((r_pred) ** 2)
 
         loss_dict = {"ics": ics_loss, "res": res_loss}