c670c5094e34042be69c4277ea5769013dbaee04
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-09-12 11:00:57 +0200

    changed default config to save every 1000

--- examples/drift_diffusion/configs/default.py
+++ examples/drift_diffusion/configs/default.py
@@ -69,7 +69,7 @@ def get_config():
 
     # Saving
     config.saving = saving = ml_collections.ConfigDict()
-    saving.save_every_steps = None
+    saving.save_every_steps = 1000
     saving.num_keep_ckpts = 10
 
     # # Input shape for initializing Flax models
