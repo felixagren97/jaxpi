94074cf34fd181e6b9cc79ada3c2e69ea4094ee1
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-09-21 14:02:32 +0200

    updated loss dict

--- examples/coupled_case/configs/no_grad_norm.py
+++ examples/coupled_case/configs/no_grad_norm.py
@@ -12,7 +12,7 @@ def get_config():
     # Weights & Biases
     config.wandb = wandb = ml_collections.ConfigDict()
     wandb.project = "PINN-Coupled-case"
-    wandb.name = "no_grad_norm"
+    wandb.name = "default"
     wandb.tag = None
 
     # Arch
@@ -20,11 +20,12 @@ def get_config():
     arch.arch_name = "Mlp"
     arch.num_layers = 4
     arch.layer_size = 256
-    arch.out_dim = 1
+    arch.out_dim = 2
     arch.activation = "tanh"
     arch.periodicity = ml_collections.ConfigDict(
         {"period": (2 * jnp.pi, 1.0), "axis": (0, 1), "trainable": (True, False)}
     )
+    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 1.0, "embed_dim": 256})
     arch.reparam = ml_collections.ConfigDict(
         {"type": "weight_fact", "mean": 1.0, "stddev": 0.1}
     )
@@ -47,9 +48,15 @@ def get_config():
 
     # Weighting
     config.weighting = weighting = ml_collections.ConfigDict()
-
     weighting.scheme = None
-    weighting.init_weights = ml_collections.ConfigDict({"ics": 1.0, "res": 1.0, "bcs": 1.0})
+    weighting.init_weights = ml_collections.ConfigDict({
+            "ics": 1.0,
+            "bcs_n": 1.0, 
+            #"bcs_inner": 1.0, Hard boundary
+            #"bcs_outer": 1.0, Hard boundary 
+            "ru": 1.0,
+            "rn": 1.0
+        })
     weighting.momentum = 0.9
     weighting.update_every_steps = 1000
 
@@ -63,13 +70,13 @@ def get_config():
     logging.log_errors = True
     logging.log_losses = True
     logging.log_weights = True
-    logging.log_preds = False
     logging.log_grads = False
     logging.log_ntk = False
+    logging.log_preds = False
 
     # Saving
     config.saving = saving = ml_collections.ConfigDict()
-    saving.save_every_steps = None
+    saving.save_every_steps = 10000
     saving.num_keep_ckpts = 10
 
     # # Input shape for initializing Flax models
