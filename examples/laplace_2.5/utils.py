800c32e5fbc0c9c1e172e5a868f636124fb87283
felixagren97 <felixagren@outlook.com>
2023-09-25 15:11:20 +0200

    created laplace-2.5

---
+++ examples/laplace_2.5/utils.py
@@ -0,0 +1,13 @@
+import jax.numpy as jnp
+from jax import vmap
+
+
+def get_dataset(r_0=0.001, r_1=1, n_r=128):
+    r_star = jnp.linspace(r_0, r_1, n_r)
+    C = 1/(jnp.log(r_0)-jnp.log(r_1))
+    A = -jnp.log(r_1)*C
+    
+    u_exact_fn = lambda r: C*jnp.log(r)+A
+    u_exact = vmap(u_exact_fn)(r_star)
+    
+    return u_exact, r_star
