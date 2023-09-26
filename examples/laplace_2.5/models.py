f7706d34a742c9267f69468a91c99312ceb5dea3
felixagren97 <felixagren@outlook.com>
2023-09-26 10:39:20 +0200

    initial model changes

--- examples/laplace_2.5/models.py
+++ examples/laplace_2.5/models.py
@@ -11,37 +11,36 @@ from matplotlib import pyplot as plt
 
 
 class Laplace(ForwardIVP):
-    def __init__(self, config, u0, u1, r_star):
+    def __init__(self, config, u0, u1, x_star):
         super().__init__(config)
 
         self.u0 = u0
         self.u1 = u1
-        self.r_star = r_star
+        self.x_star = x_star
 
-        self.r0 = r_star[0]
-        self.r1 = r_star[-1]
+        self.x0 = x_star[0]
+        self.x1 = x_star[-1]
 
         # parameters 
         self.q = 1.602e-19
         self.epsilon = 8.85e-12
 
-        #new  
+        # mappings  
         self.u_pred_fn = vmap(self.u_net, (None, 0))
         self.r_pred_fn = vmap(self.r_net, (None, 0))
 
-    def u_net(self, params, r):
-        # params = weights for NN
-        r_reshape = jnp.reshape(r, (1, -1)) # make it a 2d array with just one column to emulate jnp.stack()
-        u = self.state.apply_fn(params, r_reshape) # gives r to the neural network's (self.state) forward pass (apply_fn)
+    def u_net(self, params, x):
+        x_reshape = jnp.reshape(x, (1, -1)) # make it a 2d array with just one column to emulate jnp.stack()
+        u = self.state.apply_fn(params, x_reshape) # gives r to the neural network's (self.state) forward pass (apply_fn)
         return u[0] # soft boundary
-        #return (self.r1-r)/(self.r1-self.r0) + (r-self.r0)*(self.r1 - r)*u[0] # hard boundary
+        #return (self.x1-r)/(self.x1-self.x0) + (r-self.x0)*(self.x1 - r)*u[0] # hard boundary
 
 
-    def r_net(self, params, r):        
-        du_r = grad(self.u_net, argnums=1)(params, r)
-        du_rr = grad(grad(self.u_net, argnums=1), argnums=1)(params, r)
+    def r_net(self, params, x):        
+        du_x = grad(self.u_net, argnums=1)(params, x)
+        du_xx = grad(grad(self.u_net, argnums=1), argnums=1)(params, x)
         #du_rr = grad(lambda r: grad(self.u_net, argnums=1)(params, r))(r) #TODO: understand why this seems to work? Check if correct
-        return r * du_rr + du_r  # Scaled by r, try w/o? 
+        return x * du_xx + du_x  # Scaled by r, try w/o? 
 
     @partial(jit, static_argnums=(0,))
     def res_and_w(self, params, batch): #TODO: think should never be called
@@ -50,11 +49,11 @@ class Laplace(ForwardIVP):
     @partial(jit, static_argnums=(0,))
     def losses(self, params, batch):
         # inner boundary condition 
-        u_pred = self.u_net(params, self.r0)
+        u_pred = self.u_net(params, self.x0)
         inner_bcs_loss = jnp.mean((self.u0 - u_pred) ** 2)
 
         # outer boundary condition 
-        u_pred = self.u_net(params, self.r1)
+        u_pred = self.u_net(params, self.x1)
         outer_bcs_loss = jnp.mean((self.u1 - u_pred) ** 2)
 
         # Residual loss
@@ -69,11 +68,9 @@ class Laplace(ForwardIVP):
 
     @partial(jit, static_argnums=(0,))
     def compute_diag_ntk(self, params, batch):
-        #ics_ntk = vmap(ntk_fn, (None, None, 0))(
-        #    self.u_net, params, self.r_star
-        #)
-        inner_bcs_ntk = self.u_net(params, self.r0)
-        outer_bcs_ntk = self.u_net(params, self.r1)
+
+        inner_bcs_ntk = self.u_net(params, self.x0)
+        outer_bcs_ntk = self.u_net(params, self.x1)
 
 
         # Consider the effect of causal weights
@@ -91,7 +88,7 @@ class Laplace(ForwardIVP):
 
     @partial(jit, static_argnums=(0,))
     def compute_l2_error(self, params, u_test):
-        u_pred = self.u_pred_fn(params, self.r_star)
+        u_pred = self.u_pred_fn(params, self.x_star)
         error = jnp.linalg.norm(u_pred - u_test) / jnp.linalg.norm(u_test)
         return error
 
@@ -105,7 +102,7 @@ class LaplaceEvaluator(BaseEvaluator):
         self.log_dict["l2_error"] = l2_error
 
     def log_preds(self, params):
-        u_pred = self.model.u_pred_fn(params, self.model.r_star)
+        u_pred = self.model.u_pred_fn(params, self.model.x_star)
         fig = plt.figure(figsize=(6, 5))
         plt.imshow(u_pred.T, cmap="jet")
         self.log_dict["u_pred"] = fig
