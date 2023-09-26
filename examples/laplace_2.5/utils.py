2ac092222d892615ccb256cfadb173ed83613e51
felixagren97 <felixagren@outlook.com>
2023-09-26 11:20:34 +0200

    fixed eval

--- examples/laplace_2.5/utils.py
+++ examples/laplace_2.5/utils.py
@@ -6,7 +6,7 @@ def get_dataset(n_x):
     L = 1 # per case 2
     x_star = jnp.linspace(0, L, n_x)
     # Dummy function to replace analytical solution.
-    u_exact_fn = lambda t, x: 0
+    u_exact_fn = lambda x: 0
     u_exact = vmap(u_exact_fn)(x_star)
     
     return u_exact, x_star
