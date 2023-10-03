d8d11be294554abd1a6c6027ae51158eaf412c75
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-03 09:36:06 +0200

    lowered lr to avoid NaN

--- examples/laplace_2.5/configs/current_sota.py
+++ examples/laplace_2.5/configs/current_sota.py
@@ -32,7 +32,7 @@ def get_config():
     optim.beta1 = 0.9
     optim.beta2 = 0.999
     optim.eps = 1e-8
-    optim.learning_rate = 1e-3
+    optim.learning_rate = 1e-4
     optim.decay_rate = 0.9
     optim.decay_steps = 2000
     optim.grad_accum_steps = 0
