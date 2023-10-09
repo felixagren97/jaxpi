f1590f784f15d1601843d6f08c4ce79ea204903d
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-09 17:20:34 +0200

    last test for today

--- jaxpi/archs.py
+++ jaxpi/archs.py
@@ -169,7 +169,7 @@ class InverseMlp(nn.Module):
     
     def setup(self):
         self.activation_fn = _get_activation(self.activation)
-        self.rho_param = self.param('rho_param', lambda rng, _: jax.random.normal(rng, (1,)))        
+        self.rho_param = self.param('rho_param', jax.random.normal(jax.random.PRNGKey(0), (1,)))        
 
     @nn.compact
     def __call__(self, x):
