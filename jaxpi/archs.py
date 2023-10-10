95da73843ffe6f585f030cae9c7396fabecea85d
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-10 08:46:36 +0200

    changed output index

--- jaxpi/archs.py
+++ jaxpi/archs.py
@@ -169,7 +169,7 @@ class InverseMlp(nn.Module):
     
     def setup(self):
         self.activation_fn = _get_activation(self.activation)
-        self.rho_param = self.param('rho_param', lambda _ : jax.random.normal(jax.random.PRNGKey(0), (1,)))        
+        self.rho_param = self.param('rho_param', lambda _ : jax.random.normal(jax.random.PRNGKey(0), (1,)))  # TODO: check if this can be done in a nicer way    
 
     @nn.compact
     def __call__(self, x):
