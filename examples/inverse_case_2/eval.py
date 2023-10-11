0c345f753a904621a69a467c460e20527aab1784
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-11 13:52:22 +0200

    init commit for inverse case 2

---
+++ examples/inverse_case_2/eval.py
@@ -0,0 +1,85 @@
+import os
+
+import ml_collections
+
+import jax.numpy as jnp
+
+import matplotlib.pyplot as plt
+
+from jaxpi.utils import restore_checkpoint
+
+import models
+from utils import get_dataset
+
+
+def evaluate(config: ml_collections.ConfigDict, workdir: str):
+   # Problem setup
+    E_ext = 1e6
+    n_0 = 0.1/1e9
+    n_inj = 1
+    n_t = 200  # number of time steps TODO: Increase?
+    n_x = 128  # number of spatial points
+
+    # Get  dataset
+    u_ref, t_star, x_star = get_dataset(n_t, n_x)
+
+    # Restore model
+    model = models.DriftDiffusion(config, n_inj, n_0, E_ext, t_star, x_star)
+    ckpt_path = os.path.join(workdir, "ckpt", config.wandb.name)
+    model.state = restore_checkpoint(model.state, ckpt_path)
+    params = model.state.params
+
+    # Compute L2 error
+    l2_error = model.compute_l2_error(params, u_ref)
+    print("L2 error: {:.3e}".format(l2_error))
+
+    u_pred = model.u_pred_fn(params, model.t_star, model.x_star)
+    TT, XX = jnp.meshgrid(t_star, x_star, indexing="ij")
+    
+    print('jnp.max(u_pred):' , jnp.max(u_pred))
+    print('shape u_pred:', u_pred.shape)
+    print('shape x_star:', x_star.shape)
+    print('shape t_star:', t_star.shape)
+    
+    # Plot results
+    fig = plt.figure(figsize=(18, 5))
+    plt.subplot(1, 2, 1)
+    idx_step = int(n_t/10)
+    plt.plot(x_star, u_pred[idx_step * 0, :], label='t=0.000')
+    plt.plot(x_star, u_pred[idx_step * 1, :], label='t=0.001')
+    plt.plot(x_star, u_pred[idx_step * 2, :], label='t=0.002')
+    plt.plot(x_star, u_pred[idx_step * 3, :], label='t=0.003')
+    plt.plot(x_star, u_pred[idx_step * 4, :], label='t=0.004')
+    plt.plot(x_star, u_pred[idx_step * 5, :], label='t=0.005')
+    plt.plot(x_star, u_pred[idx_step * 6, :], label='t=0.006')
+    plt.grid()
+    plt.xlabel("x")
+    plt.ylabel("n")
+    plt.title("Charge Density over x for different timesteps")
+    plt.legend()
+    plt.tight_layout()
+
+    plt.subplot(1, 2, 2)
+    plt.pcolor(TT, XX, u_pred, cmap="jet")
+    plt.colorbar()
+    plt.xlabel("t")
+    plt.ylabel("x")
+    plt.title("Predicted")
+    plt.tight_layout()
+
+    #plt.subplot(1, 3, 3)
+    #plt.pcolor(TT, XX, jnp.abs(u_ref - u_pred), cmap="jet")
+    #plt.colorbar()
+    #plt.xlabel("t")
+    #plt.ylabel("x")
+    #plt.title("Absolute error")
+    #plt.tight_layout()
+
+    # Save the figure
+    save_dir = os.path.join(workdir, "figures", config.wandb.name)
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+
+    fig_path = os.path.join(save_dir, "drift_diffusion.pdf")
+    fig.savefig(fig_path, bbox_inches="tight", dpi=300)
+
