858df7281ac0f7ade527c4572b7f8c3a01613bbb
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-12 11:14:28 +0200

    lowered embedded scale to hopefully minimize over-shoot.

--- examples/inverse_case_2/configs/default.py
+++ examples/inverse_case_2/configs/default.py
@@ -27,7 +27,7 @@ def get_config():
     arch.out_dim = 1
     arch.activation = "gelu"
     arch.periodicity = False # ml_collections.ConfigDict( {"period": (2 * jnp.pi, 1.0), "axis": (0, 1), "trainable": (True, False)})
-    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 10.0, "embed_dim": 256})
+    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 1.0, "embed_dim": 256})
     arch.reparam = ml_collections.ConfigDict(
         {"type": "weight_fact", "mean": 1.0, "stddev": 0.1}
     )
