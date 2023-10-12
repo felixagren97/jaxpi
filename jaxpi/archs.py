88306186caa1e1d8c5e5f83e7b043bc2c047fa06
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-12 17:57:19 +0200

    changing back to random init of mu

--- jaxpi/archs.py
+++ jaxpi/archs.py
@@ -163,8 +163,8 @@ class InverseMlpOffset(Mlp):
         super().setup()  # Call the setup method of the parent class
 
         # Additional setup for InverseMlp
-        self.offset_param = self.param('offset_param', lambda rng: jax.random.uniform(jax.random.PRNGKey(rng[0]), (1,), minval=jnp.log(1e-4), maxval=jnp.log(1e-1)))  # TODO: Change to a random value in an appropriate range
-
+        self.offset_param = self.param('offset_param', lambda rng: jax.random.uniform(jax.random.PRNGKey(rng[0]), (1,), minval=jnp.log(1e-4), maxval=jnp.log(1e-1)))  
+        
 class InverseMlpRho(Mlp):
     arch_name: Optional[str] = "InverseMlpRho"
 
@@ -181,8 +181,8 @@ class InverseMlpMu(Mlp):
         super().setup()  # Call the setup method of the parent class
 
         # Additional setup for InverseMlp
-        self.offset_param = self.param('mu_param', lambda rng: jax.random.uniform(jax.random.PRNGKey(rng[0]), (1,), minval=jnp.log(2e-5), maxval=jnp.log(2e-5))) #True mu = 2e-4, Interval 2e-5 to 2e-3
-        # Filip Carlsson tutorial for git  
+        self.offset_param = self.param('mu_param', lambda rng: jax.random.uniform(jax.random.PRNGKey(rng[0]), (1,), minval=jnp.log(2e-5), maxval=jnp.log(2e-3))) 
+          
 
 class ModifiedMlp(nn.Module):
     arch_name: Optional[str] = "ModifiedMlp"
