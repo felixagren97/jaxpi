4513b02c575edd4d30cf94bddd62991ed2e1ff27
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-13 13:19:29 +0200

    corrected get_observations

--- examples/inverse_case_1_5/utils.py
+++ examples/inverse_case_1_5/utils.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
 from jax import vmap
-
+import numpy as np
+import jax
 
 def get_dataset(n_x):
     L = 1 # per case 2
@@ -13,12 +14,15 @@ def get_dataset(n_x):
 
 def get_observations(n_obs):
    # open numpy array from file called obs.dat
-   obs = jnp.load('obs.dat', allow_pickle=True)
+   obs = np.loadtxt('obs.dat')
+   obs = jnp.array(obs)
+
    obs_x = obs[:,0]
    obs_u = obs[:,1]
 
    #selct n_obs random indices
-   idx = jnp.random.randint(0, len(obs_x), n_obs)
+   key = jax.random.PRNGKey(42) 
+   idx = jax.random.randint(key, minval=0, maxval=len(obs_x), shape=(n_obs,))
    obs_x = obs_x[idx]
    obs_u = obs_u[idx]
 
