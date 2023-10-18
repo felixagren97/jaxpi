2cd734ac97aa3e5f0263fe41c571e6d556d49452
felixagren97 <felixagren@outlook.com>
2023-10-18 09:59:38 +0200

    added inverserho network to available archs

--- jaxpi/models.py
+++ jaxpi/models.py
@@ -54,6 +54,9 @@ def _create_arch(config):
     elif config.arch_name == "InverseMlpOffset":
         arch = archs.InverseMlpOffset(**config)
 
+    elif config.arch_name == "InverseMlpRho":
+        arch = archs.InverseMlpRho(**config)
+
     else:
         raise NotImplementedError(f"Arch {config.arch_name} not supported yet!")
 
