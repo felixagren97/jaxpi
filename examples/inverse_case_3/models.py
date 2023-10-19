6d694ac5905548a837f473817f6b30f9a6ce7f04
felixagren97 <felixagren@outlook.com>
2023-10-19 14:35:21 +0200

    bug fix

--- examples/inverse_case_3/models.py
+++ examples/inverse_case_3/models.py
@@ -150,8 +150,8 @@ class InverseCoupledCase(ForwardIVP):
             ru_loss = jnp.mean(ru_pred**2)
             rn_loss = jnp.mean(rn_pred**2)
             
-            obs_u_pred = self.u_net(self.obs_t, self.obs_x)
-            obs_n_pred = self.n_net(self.obs_t, self.obs_x)
+            obs_u_pred = self.u_net(params, self.obs_t, self.obs_x)
+            obs_n_pred = self.n_net(params, self.obs_t, self.obs_x)
             #obs_u_pred = self.u_pred_fn(params, self.obs_t, self.obs_x)
             #obs_n_pred = self.n_pred_fn(params, self.obs_t, self.obs_x)
             obs_u_loss = jnp.mean((self.obs_u - obs_u_pred)**2)
