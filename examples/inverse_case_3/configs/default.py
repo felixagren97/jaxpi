02fecbed1fa41f86ed0b1b75de90ef14a2ca4786
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-10-19 12:49:23 +0200

    changed default data file

--- examples/inverse_case_3/configs/default.py
+++ examples/inverse_case_3/configs/default.py
@@ -19,7 +19,7 @@ def get_config():
 
     setting.true_mu = 2e-4
 
-    setting.obs_file = 'case3_obs.dat'
+    setting.obs_file = 'case3_obs_mu_2e-4_n_1e9.dat'
     setting.n_obs = 1000
 
     # Weights & Biases
