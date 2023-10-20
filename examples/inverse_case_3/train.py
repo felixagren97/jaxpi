f6eca795ea885fe582ce3f55e2d7c3b5bf9aaabd
felixagren97 <felixagren@outlook.com>
2023-10-20 09:58:15 +0200

    printed real mu value instead of log(mu)

--- examples/inverse_case_3/train.py
+++ examples/inverse_case_3/train.py
@@ -71,7 +71,7 @@ def train_and_evaluate(config: ml_collections.ConfigDict, workdir: str):
                 batch = jax.device_get(tree_map(lambda x: x[0], batch))
                 log_dict = evaluator(state, batch, u_ref, n_ref)
                 
-                mu = state.params['params']['mu_param'][0]
+                mu = jnp.exp(state.params['params']['mu_param'][0])
                 log_dict["mu_param"] = mu
                 wandb.log(log_dict, step)
                 end_time = time.time()
