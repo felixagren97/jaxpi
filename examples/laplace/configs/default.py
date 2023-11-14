740bbf2b2a2c8fca80b9a159b0e2839637012a7e
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-11-14 10:56:45 +0100

    bug fix

--- examples/laplace/configs/default.py
+++ examples/laplace/configs/default.py
@@ -56,7 +56,7 @@ def get_config():
     # Weighting
     config.weighting = weighting = ml_collections.ConfigDict()
     weighting.scheme = "grad_norm"
-    weighting.init_weights = ml_collections.ConfigDict({"ics": 1.0, "res": 1.0})
+    weighting.init_weights = ml_collections.ConfigDict({"res": 1.0})
     weighting.momentum = 0.9
     weighting.update_every_steps = 1000
 
