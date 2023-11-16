ef8ffbe32224a319f66e97c715e658ab51260799
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-11-16 10:22:35 +0100

    updated specified sweep configs.

--- examples/laplace/configs/sweep.py
+++ examples/laplace/configs/sweep.py
@@ -9,10 +9,17 @@ def get_config():
 
     config.mode = "train"
 
+    config.setting = setting = ml_collections.ConfigDict()
+    setting.r_0 = 0.0001
+    setting.r_1 = 0.5
+    setting.u_0 = 1
+    setting.u_1 = 0
+    config.setting.n_r = 12_000 #TODO: Reduce. 
+
     # Weights & Biases
     config.wandb = wandb = ml_collections.ConfigDict()
     wandb.project = "PINN-Laplace"
-    wandb.name = None
+    wandb.name = "default"
     wandb.tag = None
 
     # Arch
@@ -23,9 +30,9 @@ def get_config():
     arch.out_dim = 1
     arch.activation = "tanh"
     arch.periodicity = ml_collections.ConfigDict(
-        {"period": (2 * jnp.pi, 1.0), "axis": (0, 1), "trainable": (True, False)}
+        {"period": (1.0,), "axis": (1,), "trainable": (False,)} 
     )
-    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 1.0, "embed_dim": 256})
+    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 10.0, "embed_dim": 256})
     arch.reparam = ml_collections.ConfigDict(
         {"type": "weight_fact", "mean": 1.0, "stddev": 0.1}
     )
@@ -38,29 +45,24 @@ def get_config():
     optim.eps = 1e-8
     optim.learning_rate = 1e-3
     optim.decay_rate = 0.9
-    optim.decay_steps = 5000
+    optim.decay_steps = 2000
     optim.grad_accum_steps = 0
 
     # Training
     config.training = training = ml_collections.ConfigDict()
-    training.max_steps = 400000
-    training.batch_size_per_device = 4096
-
-    # Saving
-    config.saving = saving = ml_collections.ConfigDict()
-    saving.save_every_steps = None
-    saving.num_keep_ckpts = None
+    training.max_steps = 200_000
+    training.batch_size_per_device = 516
 
     # Weighting
     config.weighting = weighting = ml_collections.ConfigDict()
     weighting.scheme = "grad_norm"
-    weighting.init_weights = ml_collections.ConfigDict({"ics": 1.0, "res": 1.0})
+    weighting.init_weights = ml_collections.ConfigDict({"res": 1.0})
     weighting.momentum = 0.9
     weighting.update_every_steps = 1000
 
-    weighting.use_causal = True
+    weighting.use_causal = False # TODO: verify: was true, but changed to false as no temporal domain
     weighting.causal_tol = 1.0
-    weighting.num_chunks = 16
+    weighting.num_chunks = 32
 
     # Logging
     config.logging = logging = ml_collections.ConfigDict()
@@ -74,10 +76,11 @@ def get_config():
 
     # Saving
     config.saving = saving = ml_collections.ConfigDict()
-    saving.save_every_steps = None
+    saving.save_every_steps = False
+    saving.plot = False
 
     # # Input shape for initializing Flax models
-    config.input_dim = 2
+    config.input_dim = 1
 
     # Integer for PRNG random seed.
     config.seed = 42
