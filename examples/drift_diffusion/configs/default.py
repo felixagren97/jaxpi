4c470912ea5334b8d76bad34d9bd98baa9bae328
felixagren97 <felixagren@outlook.com>
2023-11-16 13:33:19 +0100

    Set periodicity to False

--- examples/drift_diffusion/configs/default.py
+++ examples/drift_diffusion/configs/default.py
@@ -29,9 +29,7 @@ def get_config():
     arch.layer_size = 256
     arch.out_dim = 1
     arch.activation = "tanh"
-    arch.periodicity = ml_collections.ConfigDict(
-        {"period": (2 * jnp.pi, 1.0), "axis": (0, 1), "trainable": (True, False)}
-    )
+    arch.periodicity = False
     arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 1.0, "embed_dim": 256})
     arch.reparam = ml_collections.ConfigDict(
         {"type": "weight_fact", "mean": 1.0, "stddev": 0.1}
