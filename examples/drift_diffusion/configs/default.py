5f736ce89de6a94d9e662c84f50b7aa7ec609f35
felixagren97 <felixagren@outlook.com>
2023-11-16 14:35:04 +0100

    changed saved_every to 10k (from 5k)

--- examples/drift_diffusion/configs/default.py
+++ examples/drift_diffusion/configs/default.py
@@ -74,7 +74,7 @@ def get_config():
 
     # Saving
     config.saving = saving = ml_collections.ConfigDict()
-    saving.save_every_steps = 5000
+    saving.save_every_steps = 10_000
     saving.num_keep_ckpts = 1
     saving.plot = True
 
