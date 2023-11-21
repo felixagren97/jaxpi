09fcb326ea771e09f702abd5f85c82d933254ce2
felixagren97 <felixagren@outlook.com>
2023-11-21 10:59:57 +0100

    bug fix

--- examples/inverse_case_1/configs/plain.py
+++ examples/inverse_case_1/configs/plain.py
@@ -31,7 +31,7 @@ def get_config():
 
     # Arch
     config.arch = arch = ml_collections.ConfigDict()
-    arch.arch_name = "Mlp"
+    arch.arch_name = "InverseMlpRho"
     arch.num_layers = 6
     arch.layer_size = 256
     arch.out_dim = 1
