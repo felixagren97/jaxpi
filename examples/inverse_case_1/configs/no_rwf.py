59793345bf5e8791a3c5428ad67413f6cfd7d66d
felixagren97 <felixagren@outlook.com>
2023-11-22 10:15:45 +0100

    changed back to lr 1e-3, seems to be working once scaled to 1e-8

--- examples/inverse_case_1/configs/no_rwf.py
+++ examples/inverse_case_1/configs/no_rwf.py
@@ -48,7 +48,7 @@ def get_config():
     optim.beta1 = 0.9
     optim.beta2 = 0.999
     optim.eps = 1e-8
-    optim.learning_rate = 5e-4
+    optim.learning_rate = 1e-3
     optim.decay_rate = 0.9
     optim.decay_steps = 2000
     optim.grad_accum_steps = 0
@@ -63,7 +63,7 @@ def get_config():
     weighting.scheme = "grad_norm"
     weighting.init_weights = ml_collections.ConfigDict({"res": 1.0, "observ": 1.0})
     weighting.momentum = 0.9
-    weighting.update_every_steps = 2000
+    weighting.update_every_steps = 1000
 
     weighting.use_causal = False
     weighting.causal_tol = 1.0
