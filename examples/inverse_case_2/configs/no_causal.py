013b689c2f2b980bafafd90a4abb6411e62fe3c9
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-12-01 13:35:14 +0100

    changed name for no_causal as well

--- examples/inverse_case_2/configs/no_causal.py
+++ examples/inverse_case_2/configs/no_causal.py
@@ -22,7 +22,7 @@ def get_config():
     # Weights & Biases
     config.wandb = wandb = ml_collections.ConfigDict()
     wandb.project = "PINN-Inverse-Case-2"
-    wandb.name = "default"
+    wandb.name = "no_causal"
     wandb.tag = None
 
     # Arch
