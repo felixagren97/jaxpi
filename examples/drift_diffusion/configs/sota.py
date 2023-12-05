20971b69adfdd7f2ee865ebd8498ab9b2633db0d
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-12-05 23:25:59 +0100

    no checkpointing

--- examples/drift_diffusion/configs/sota.py
+++ examples/drift_diffusion/configs/sota.py
@@ -72,7 +72,7 @@ def get_config():
 
     # Saving
     config.saving = saving = ml_collections.ConfigDict()
-    saving.save_every_steps = 10_000
+    saving.save_every_steps = None
     saving.num_keep_ckpts = 1
     saving.plot = True
 
