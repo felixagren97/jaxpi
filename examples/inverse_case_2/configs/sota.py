386577a94a5fb53799e0ee51338e625b25c2d22e
Carl-Johan Björnson <calle.bjornson@gmail.com>
2023-12-06 10:45:54 +0100

    changed default value to enable overriding

--- examples/inverse_case_2/configs/sota.py
+++ examples/inverse_case_2/configs/sota.py
@@ -12,7 +12,7 @@ def get_config():
     # Setting
     config.setting = setting = ml_collections.ConfigDict()
     setting.true_mu = 2e-4
-    setting.noise_level = None
+    setting.noise_level = 0.01
     setting.n_t_obs = 100 
     setting.n_x_obs = 64
     setting.E_ext = 1e6
