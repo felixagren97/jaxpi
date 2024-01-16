4cf4ecdd57b14e47d72e6d7eb0b863ce668a1595
Carl-Johan Björnson <calle.bjornson@gmail.com>
2024-01-16 14:36:31 +0100

    testing extraction of sample info to samplers.py

--- examples/laplace/train.py
+++ examples/laplace/train.py
@@ -77,16 +77,16 @@ def train_and_evaluate(config: ml_collections.ConfigDict, workdir: str):
     for step in range(config.training.max_steps):
         
         # Update RAD points
-        if step % config.setting.resample_every_steps == 0 and step != 0:
+        if step % config.sampler.resample_every_steps == 0 and step != 0:
             
-            if config.sampler.sampler_name == "rad":
-                state = jax.device_get(tree_map(lambda x: x[0], model.state))
-                params = state.params
-                r_eval = jnp.linspace(r_0, r_1, 10_000)
-                res_pred = jnp.abs(model.r_pred_fn(params, r_eval)) # Verify shape on r_eval
-                norm_r_eval = res_pred / jnp.sum(res_pred)
+            #if config.sampler.sampler_name == "rad":
+                #state = jax.device_get(tree_map(lambda x: x[0], model.state))
+                #params = state.params
+                #r_eval = jnp.linspace(r_0, r_1, 10_000)
+                #res_pred = jnp.abs(model.r_pred_fn(params, r_eval)) # Verify shape on r_eval
+                #norm_r_eval = res_pred / jnp.sum(res_pred)
 
-            sampler = init_sampler(config.sampler.sampler_name, x=r_eval, probs=norm_r_eval, batch_size=config.training.batch_size_per_device)
+            sampler = init_sampler(model, config.sampler.sampler_name, batch_size=config.training.batch_size_per_device)
             res_sampler = iter(sampler)
             #res_sampler = iter(OneDimensionalRadSampler(r_eval, norm_r_eval, config.training.batch_size_per_device))
             
--- jaxpi/samplers.py
+++ jaxpi/samplers.py
@@ -1,17 +1,21 @@
 from abc import ABC, abstractmethod
 from functools import partial
 
+import jax
 import jax.numpy as jnp
 from jax import random, pmap, local_device_count
+from jax.tree_util import tree_map
 
 from torch.utils.data import Dataset
 
 
 # Function for initializing sampler from config file
 # argument: sampler name from config file 
-def init_sampler(sampler: str, **kwargs):
+def init_sampler(model, sampler: str, **kwargs):
     if sampler == "rad":
-        return OneDimensionalRadSampler(**kwargs)
+        return OneDimensionalRadSampler(model, **kwargs)
+    elif sampler == "rad2":
+        return OneDimensionalRadSamplerTwo(**kwargs)
     else:     
         raise NotImplementedError(f"Sampler {sampler} not implemented!")
 
@@ -53,16 +57,43 @@ class UniformSampler(BaseSampler):
 
 # 
 class OneDimensionalRadSampler(BaseSampler):
-    def __init__(self, x, probs, batch_size, rng_key=random.PRNGKey(1234)):
+    def __init__(self, model, x, probs, batch_size, rng_key=random.PRNGKey(1234)):
         super().__init__(batch_size, rng_key)
         self.dim = 1
         self.x = x
         self.probs = probs
+        self.model = model 
 
     @partial(pmap, static_broadcasted_argnums=(0,))
     def data_generation(self, key):
         "Generates data containing batch_size samples"
-        batch = random.choice(key, self.x, shape=(self.batch_size,), p=self.probs) 
+        state = jax.device_get(tree_map(lambda x: x[0], self.model.state))
+        params = state.params
+        r_eval = jnp.linspace(0.001, 0.5, 10_000)
+        res_pred = jnp.abs(self.model.r_pred_fn(params, r_eval)) # Verify shape on r_eval
+        norm_r_eval = res_pred / jnp.sum(res_pred)
+        
+        batch = random.choice(key, r_eval, shape=(self.batch_size,), p=norm_r_eval) 
+        batch = batch.reshape(-1,1)
+        return batch
+
+class OneDimensionalRadSamplerTwo(BaseSampler):
+    # Imporved RAD
+
+    def __init__(self, x, y_hat, batch_size, c, k, rng_key=random.PRNGKey(1234)):
+        super().__init__(batch_size, rng_key)
+        self.dim = 1
+        self.x = x
+        self.y_hat = y_hat
+        self.c = c 
+        self.k = k
+
+    @partial(pmap, static_broadcasted_argnums=(0,))
+    def data_generation(self, key):
+        "Generates data containing batch_size samples"
+        prob = jnp.power(self.y_hat, self.k) / jnp.power(self.y_hat, self.k).mean() + self.c
+        norm_prob = prob / prob.sum()
+        batch = random.choice(key, self.x, shape=(self.batch_size,), p=norm_prob) 
         batch = batch.reshape(-1,1)
         return batch
 
