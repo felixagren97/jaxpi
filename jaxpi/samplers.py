b5a9b811ff6571f8c72febb422d7a28c7419a4e1
Carl-Johan Björnson <calle.bjornson@gmail.com>
2024-01-23 15:53:56 +0100

    testing with res[0] instead of .shape

--- jaxpi/samplers.py
+++ jaxpi/samplers.py
@@ -197,8 +197,8 @@ class RadCosineAnnealing(BaseSampler):
         jax.debug.print("🤯 {x} 🤯", x=num_res)
         jax.debug.print("🤯 {x} 🤯", x=num_uniform)
         
-        res_batch = random.choice(key, self.r_eval, shape=(num_res.shape[0],), p=self.current_prob) 
-        uni_batch = random.uniform(key, shape=(num_uniform.shape[0], ), minval=self.r_eval[0], maxval=self.r_eval[-1])
+        res_batch = random.choice(key, self.r_eval, shape=(num_res[0],), p=self.current_prob) 
+        uni_batch = random.uniform(key, shape=(num_uniform[0], ), minval=self.r_eval[0], maxval=self.r_eval[-1])
         batch = jnp.concatenate([res_batch, uni_batch], axis=0)
 
         batch = batch.reshape(-1, 1)
