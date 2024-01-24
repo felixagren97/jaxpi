f7da5cfc155730c4690df1c426e83977a8eaa550
Carl-Johan Björnson <calle.bjornson@gmail.com>
2024-01-24 09:57:48 +0100

    same strategy

--- jaxpi/samplers.py
+++ jaxpi/samplers.py
@@ -194,8 +194,8 @@ class RadCosineAnnealing(BaseSampler):
         num_uniform, num_res = self.get_n()
         #num_uniform = jnp.floor(self.n * self.batch_size) - 1
         #num_res = self.batch_size - num_uniform + 1
-        res_int = jnp.floor(num_res).astype(int)
-        uni_int = jnp.floor(num_uniform).astype(int)
+        res_int = int(num_res)
+        uni_int = int(num_uniform)
 
         jax.debug.print("num_res {x} 🤯", x=num_res)
         jax.debug.print("num_uniform {x} 🤯", x=num_uniform)
