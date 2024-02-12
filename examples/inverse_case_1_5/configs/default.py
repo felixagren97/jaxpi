0f59006c1ccbccd5e07caf85c9162fc23cd44ae3
Carl-Johan Björnson <calle.bjornson@gmail.com>
2024-02-12 14:55:06 +0100

    disabled checkpointing

--- examples/inverse_case_1_5/configs/default.py
+++ examples/inverse_case_1_5/configs/default.py
@@ -97,9 +97,9 @@ def get_config():
 
     # Saving
     config.saving = saving = ml_collections.ConfigDict()
-    saving.save_every_steps = 25_000
-    saving.num_keep_ckpts = 1
-    saving.plot = True
+    saving.save_every_steps = None #25_000
+    #saving.num_keep_ckpts = 1
+    saving.plot = False
 
     # # Input shape for initializing Flax models
     config.input_dim = 1
